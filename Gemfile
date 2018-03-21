03ade6213cefd850a2beb213c51791ca7b35d9a1
Abhishek kant <abhi1206118@gmail.com>
2018-03-22 01:10:03 +0800

    sidekiq included

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'deface'
 gem 'slim'
 gem 'remotipart', '~> 1.2'
 gem 'simple_form'
+gem 'sidekiq'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/jobs/import_products_job.rb
+++ app/jobs/import_products_job.rb
@@ -2,7 +2,6 @@ class ImportProductsJob < ApplicationJob
   queue_as :default
 
   def perform(import_product_task_id, first_row_index, last_row_index)
-    # ImportProducts.new(import_product_task_id, first_row_index, last_row_index).run
-    # Do something later
+    ImportProductsService.new(import_product_task_id, first_row_index, last_row_index).run
   end
 end
