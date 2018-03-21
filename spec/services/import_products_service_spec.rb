f590564d1fc9c589e3fa546ac6c1660dfa82cae0
Abhishek kant <abhi1206118@gmail.com>
2018-03-22 00:55:02 +0800

    import_products_service spec refactored

--- spec/services/import_products_service_spec.rb
+++ spec/services/import_products_service_spec.rb
@@ -14,44 +14,53 @@ RSpec.describe ImportProductsService do
       expect{ import_service.run }.to change{ Spree::Product.count }.by(3)
     end
 
-    it "sets product attributes correctly" do
-      import_service.run
-      product = Spree::Product.find_by(slug: csv_first_row_as_hash[:slug])
-      expect(product.name).to eq(csv_first_row_as_hash[:name])
-      expect(product.description).to eq(csv_first_row_as_hash[:description])
-      expect(product.available_on).to eq(csv_first_row_as_hash[:available_on])
-    end
+    describe "product assignments" do
 
-    it "sets correct category" do
-      import_service.run
-      product = Spree::Product.find_by(slug: csv_first_row_as_hash[:slug])
-      category = Spree::Taxon.find_by(name: csv_first_row_as_hash[:category])
-      expect(product.taxons).to include(category)
-    end
+      before(:each) do
+        import_service.run
+        @product = Spree::Product.find_by(slug: csv_first_row_as_hash[:slug])
+      end
 
-    it "sets correct shipping category" do
-      import_service.run
-      product = Spree::Product.find_by(slug: csv_first_row_as_hash[:slug])
-      shipping_category = Spree::ShippingCategory.find_by(name: csv_first_row_as_hash[:shipping_category])
-      expect(product.shipping_category).to eq shipping_category
-    end
+      it "sets product attributes correctly" do
+
+        expect(@product.name).to eq(csv_first_row_as_hash[:name])
+        expect(@product.description).to eq(csv_first_row_as_hash[:description])
+        expect(@product.available_on).to eq(csv_first_row_as_hash[:available_on])
+      end
+
+      it "sets correct category" do
+        import_service.run
+        @product = Spree::Product.find_by(slug: csv_first_row_as_hash[:slug])
+        category = Spree::Taxon.find_by(name: csv_first_row_as_hash[:category])
+        expect(@product.taxons).to include(category)
+      end
+
+      it "sets correct shipping category" do
+        import_service.run
+        @product = Spree::Product.find_by(slug: csv_first_row_as_hash[:slug])
+        shipping_category = Spree::ShippingCategory.find_by(name: csv_first_row_as_hash[:shipping_category])
+        expect(@product.shipping_category).to eq shipping_category
+      end
 
 
-    it "sets correct option types" do
-      import_service.run
-      product = Spree::Product.find_by(slug: csv_first_row_as_hash[:slug])
-      csv_first_row_as_hash[:option_types].split("|").each do |option_type_name|
-        option_type = Spree::OptionType.find_or_initialize_by(name: option_type_name)
-        expect(product.option_types).to include(option_type)
+      it "sets correct option types" do
+        import_service.run
+        @product = Spree::Product.find_by(slug: csv_first_row_as_hash[:slug])
+        csv_first_row_as_hash[:option_types].split("|").each do |option_type_name|
+          option_type = Spree::OptionType.find_or_initialize_by(name: option_type_name)
+          expect(@product.option_types).to include(option_type)
+        end
+      end
+
+
+      it "creates a variant for product" do
+        import_service.run
+        @product = Spree::Product.find_by(slug: csv_first_row_as_hash[:slug])
+        expect(@product.variants).not_to be_empty
       end
     end
 
 
-    it "creates a variant for product" do
-      import_service.run
-      product = Spree::Product.find_by(slug: csv_first_row_as_hash[:slug])
-      expect(product.variants).not_to be_empty
-    end
 
 
   end
